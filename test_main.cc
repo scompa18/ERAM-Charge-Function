0c617ec7f31c7300a98ef6677c7ca840e47b1244
luca.scomparin <luca.scomparin@kit.edu>
2023-03-23 18:04:24 +0100

    Added comparison mathematica notebook

--- test_main.cc
+++ test_main.cc
@@ -12,11 +12,11 @@ int main(int argc, char *argv[]) {
     // This should be the integral of Ei(-(x^2+y^2)) in a
     // square of side one centered around the origin
     cout << ion_charge_function(
-        0., 1., // Center of pad
-        1., 1., // random pad length
-        0.24,   // k such that it kills the factor of four
-        1.1,    // t that does not bother
-        0.,     // m that does not bother
+        4., 3., // Center of pad
+        2., 1., // random pad length
+        1.24,   // k such that it kills the factor of four
+        4.,     // t that does not bother
+        2.,     // m that does not bother
         1.      // T that does not bother us
     ) << endl;
 
